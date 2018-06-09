fe3b89d71cc2bb71b2d759426c128f0d1870ef71
TeamBool <39705802+TeamBool@users.noreply.github.com>
2018-06-09 08:57:42 +0200

    Add files via upload

---
+++ src_v2/client.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void printError(const char *msg)
+{
+    fprintf(stderr, "%s", msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd = 0;
+	int portno = 0;
+	int n = 0;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+    int buffersize = 0;
+    char *buffer = NULL;
+
+	/** TOFIX **/
+    int i = 0;	//Laufvariable buffer[]
+    int j = 3;	//Laufvariable argv[]
+	/** TOFIX **/
+
+	//Check for arguments
+    if (argc < 4)
+    {
+    	printError("usage %s hostname port filenames\n");
+    }
+
+	//Convert portnumber to int
+	if(strlen(argv[2]) > 5)
+	{
+		printError("Invalid portnumber!\n");
+	}
+
+	for(int i = 0; i < strlen(argv[2]); i++)
+	{
+        if(!(argv[2][i] >= '0' && argv[2][i] <= '9'))
+        {
+        	printError("Invalid portnumber!\n");
+        }
+	}
+
+    portno = atoi(argv[2]);
+
+    if(portno > 65535)
+    {
+    	printError("Invalid portnumber!\n");
+    }
+    
+    printf("Port: %d\n", portno);
+
+    //Initialize structure with \0
+    memset(&serv_addr, '\0', sizeof(serv_addr));
+
+    //Create Socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Sockfd: %d\n", sockfd);
+    if (sockfd < 0)
+    {
+    	error("ERROR opening socket");
+    }
+
+    //Resolve hostname
+    server = gethostbyname(argv[1]);
+    if(server == NULL)
+    {
+    	printError("ERROR, no such host\n");
+    }
+
+    //Fill needed structure with information
+    printf("Server: %s\n", server->h_name);
+    serv_addr.sin_family = AF_INET;
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr_list[0], server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    //Make IP printable
+    char ipaddr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &serv_addr.sin_addr.s_addr, ipaddr, INET_ADDRSTRLEN);
+    printf("IP: %s\n", ipaddr);
+
+	/** TOFIX **/
+	while(j < argc)
+	{
+		while (i < strlen(argv[j]))
+		{
+			if(argv[j][i] == '@')
+			{
+				error("Invalid filename!");
+			}
+			i++;
+		}
+		j++;
+	};
+	i = 0;
+	j = 3;
+	/** TOFIX **/
+
+    //Connect
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+    	error("ERROR connecting");
+    }
+
+    //Calculate buffersize
+    for(int i = 3; i < argc; i++)
+    {
+    	buffersize = buffersize + strlen(argv[i])+1;
+    }
+    printf("Buffersize: %d\n", buffersize);
+
+    //Allocate buffer
+    buffer = calloc(1, buffersize * sizeof(char)+1);
+	if(buffer == NULL)
+	{
+		printf("Something went wrong while allocating memory!\n");
+		exit(1);
+	}
+
+    //Fill buffer
+    for(int i = 3; i < argc; i++)
+    {
+    	strcat(buffer, argv[i]);
+    	strcat(buffer, "@");
+    }
+    printf("Buffer: %s\n", buffer);
+
+    //Send buffer
+    n = write(sockfd, buffer,strlen(buffer));
+	if (n < 0)
+	{
+		error("ERROR writing to socket");
+	}
+
+	/** TOFIX **/
+	bzero(buffer, buffersize);
+	free(buffer);
+	buffersize = 255 * sizeof(char)+1;
+	buffer = calloc(1, buffersize);
+
+	n = read(sockfd,buffer,buffersize);
+	printf("Buffer: %s\n\n", buffer);
+
+	bzero(buffer, buffersize);
+	n = read(sockfd, buffer, buffersize);
+
+	printf("Buffer: %s\n\n", buffer);
+
+	//Output
+	while(i < buffersize)
+	{
+		if(buffer[i] == '0')
+		{
+			printf("Die Datei %s existiert NICHT.\n", argv[j]);
+		} else if(buffer[i] == '1')
+		{
+			printf("Die Datei %s existiert.\n", argv[j]);
+		}
+
+		i++;
+		j++;
+	}
+	/** TOFIX **/
+
+
+
+	free(buffer);
+
+    return 0;
+}
---
+++ src_v2/server.c
@@ -0,0 +1,122 @@
+
+/*A simple server in the internet domain using TCP
+  The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+
+	FILE *fp;		// Filepointer, Pruefen ob Dateien existieren
+	char *aufteilung;
+
+	int sockfd = 0;
+	int newsockfd = 0;
+	int portno = 0;
+	socklen_t clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n = 0;
+	if (argc < 2)
+	{
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	  error("ERROR on binding");
+
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	for(;;)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+		//BEGIN get active socket port
+		struct sockaddr_in sin;
+		socklen_t len = sizeof(sin);
+		if (getsockname(newsockfd, (struct sockaddr *)&sin, &len) == -1)
+		{
+			error("getsockname");
+		}
+		else
+		{
+			printf("port number %d\n", ntohs(sin.sin_port));
+		}
+		//END get active socket port
+
+		if (newsockfd < 0)
+		{
+			error("ERROR on accept");
+		}
+
+		bzero(buffer,256);
+
+		//DATEN VOM CLIENT EMPFANGEN
+		n = read(newsockfd,buffer,255);
+
+		if (n < 0) error("ERROR reading from socket");
+
+		printf("Here is the message: %s\n\n",buffer);
+
+		n = write(newsockfd, "I got your message.\n",255);
+
+		if (n < 0) error("ERROR writing to socket");
+
+		// Buffer wieder aufteilen, pruefen und ausgeben
+		/** TOFIX **/
+		int i = 0; // Laufvariable
+		char newbuffer[256];
+		bzero(newbuffer, 256);
+		/** TOFIX **/
+		aufteilung = strtok(buffer, "@");
+		do
+		{
+			printf("Moeglicher Dateiname gefunden: %s\n", aufteilung);
+
+			fp = fopen(aufteilung, "r");
+			if(fp == NULL)
+			{
+					printf("Die Datei %s existiert NICHT.\n\n", aufteilung);
+					newbuffer[i] = '0';
+			} else
+			{
+					printf("Die Datei %s existiert.\n\n", aufteilung);
+					newbuffer[i] = '1';
+					fclose(fp);
+			}
+
+			aufteilung = strtok(NULL, "@");
+			i++;
+
+		} while(aufteilung != NULL);
+
+		n = write(newsockfd, newbuffer, 255);
+
+		if (n < 0) error("ERROR writing to socket");
+
+		close(newsockfd);
+	}
+	close(sockfd);
+	return 0;
+}
