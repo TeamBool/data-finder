86e8d9fc8dbe52392c8e54fa1425ce208cfc547b
Flyingsky <byebyesky8@gmail.com>
2018-06-11 00:50:28 +0200

    Added a bunch of stuff

--- src/client.c
+++ src/client.c
@@ -8,18 +8,22 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+#define FILENAME_START 4
+
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-void printError(const char *msg) {
+void printError(const char *msg)
+{
     fprintf(stderr, "%s", msg);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int sockfd = 0;
 	int portno = 0;
 	int n = 0;
@@ -27,46 +31,83 @@ int main(int argc, char *argv[]) {
 	struct hostent *server;
 
     int buffersize = 0;
+    char status[50];
     char *buffer = NULL;
+    char *fileBytes = calloc(1, 10 * sizeof(char)+1);
 
 	//Check for arguments
-    if (argc < 4) {
-       printError("usage %s hostname port filenames\n");
+    if (argc < 4)
+    {
+    	printError("usage %s hostname port numberOfBytes filenames\n");
     }
 
-	//Convert portnumber to int
-	if(strlen(argv[2]) > 5) {
+    //Check for control character in filename
+    for(int i = FILENAME_START; i < argc; i++) {
+        if(strstr(argv[i], "<"))
+        {
+            printError("< is not a valid filename character!\n");
+            
+        }
+    }
+
+    //Check if N bytes is valid
+    if(strlen(argv[3]) > 2)
+	{
+		printError("Invalid number of bytes!\n");
+	}
+
+    for(int i = 0; i < strlen(argv[3]); i++)
+	{
+        if(!(argv[2][i] >= '0' && argv[3][i] <= '9'))
+        {
+        	printError("Invalid number of bytes!!\n");
+        }
+	}
+
+    if(atoi(argv[3]) > 10) {
+        printError("Invalid number of bytes!!\n");
+    }
+
+	//Check and convert portnumber to int
+	if(strlen(argv[2]) > 5)
+	{
 		printError("Invalid portnumber!\n");
 	}
 
-	for(int i = 0; i < strlen(argv[2]); i++) {
-        if(!(argv[2][i] >= '0' && argv[2][i] <= '9')){
-            printError("Invalid portnumber!\n");
+	for(int i = 0; i < strlen(argv[2]); i++)
+	{
+        if(!(argv[2][i] >= '0' && argv[2][i] <= '9'))
+        {
+        	printError("Invalid portnumber!\n");
         }
 	}
 
     portno = atoi(argv[2]);
 
-    if(portno > 65535) {
-        printError("Invalid portnumber!\n");
+    if(portno > 65535)
+    {
+    	printError("Invalid portnumber!\n");
     }
     
     printf("Port: %d\n", portno);
 
     //Initialize structure with \0
     memset(&serv_addr, '\0', sizeof(serv_addr));
+    memset(status, '\0', sizeof(status));
 
     //Create Socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     printf("Sockfd: %d\n", sockfd);
-    if (sockfd < 0) {
-        error("ERROR opening socket");
+    if (sockfd < 0)
+    {
+    	error("ERROR opening socket");
     }
 
     //Resolve hostname
     server = gethostbyname(argv[1]);
-    if(server == NULL) {
-        printError("ERROR, no such host\n");
+    if(server == NULL)
+    {
+    	printError("ERROR, no such host\n");
     }
 
     //Fill needed structure with information
@@ -81,34 +122,79 @@ int main(int argc, char *argv[]) {
     printf("IP: %s\n", ipaddr);
 
     //Connect
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
-        error("ERROR connecting");
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+    	error("ERROR connecting");
+    }
+    
+    //Send number of bytes
+    n = write(sockfd, argv[3], strlen(argv[3]));
+    usleep(100);
+
+    n = read(sockfd, status, sizeof(status));
+    if(strcmp("OK", status)) {
+        printf("%s\n", status);
+        printError("Something went wrong!\n");
     }
 
     //Calculate buffersize
-    for(int i = 3; i < argc; i++) {
-        buffersize = buffersize + strlen(argv[i])+1;
+    for(int i = FILENAME_START; i < argc; i++)
+    {
+    	buffersize = buffersize + strlen(argv[i])+1;
     }
     printf("Buffersize: %d\n", buffersize);
 
     //Allocate buffer
     buffer = calloc(1, buffersize * sizeof(char)+1);
-	if(buffer == NULL) {
+	if(buffer == NULL)
+	{
 		printf("Something went wrong while allocating memory!\n");
 		exit(1);
 	}
 
     //Fill buffer
-    for(int i = 3; i < argc; i++) {
-        strcat(buffer, argv[i]);
-        strcat(buffer, "@");
+    for(int i = FILENAME_START; i < argc; i++)
+    {
+    	strcat(buffer, argv[i]);
+    	strcat(buffer, "<");
     }
-    printf("Buffer: %s\n", buffer);
+    buffer[strlen(buffer)-1] = '\0';
+    printf("SendBuffer: %s\n", buffer);
 
     //Send buffer
     n = write(sockfd, buffer,strlen(buffer));
-	if (n < 0) {
+	if (n < 0)
+	{
 		error("ERROR writing to socket");
+	}
+
+	memset(buffer, '\0', buffersize);
+	free(buffer);
+
+	buffersize = 255 * sizeof(char)+1;
+	buffer = calloc(1, buffersize);
+
+	n = read(sockfd,buffer,buffersize);
+	printf("ReceiveBuffer: %s\n\n", buffer);
+
+	memset(buffer, '\0',  buffersize);
+	n = read(sockfd, buffer, buffersize);
+    printf("Buffer: %s\n\n", buffer);
+
+	//Output
+	for(int i = 0; i < strlen(buffer); i++)
+	{
+		if(buffer[i] == '0')
+		{
+			printf("Die Datei %s existiert NICHT.\n", argv[i+FILENAME_START]);
+		} else if(buffer[i] == '1')
+		{
+			printf("Die Datei %s existiert.\n", argv[i+FILENAME_START]);
+            memset(fileBytes, '\0', buffersize);
+            n = read(sockfd, fileBytes, buffersize);
+            printf("Inhalt %s: %s\n", argv[i+FILENAME_START], fileBytes);
+		}
+        printf("\n");
 	}
 
 	free(buffer);
--- src/server.c
+++ src/server.c
@@ -1,5 +1,5 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/*A simple server in the internet domain using TCP
+  The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,11 +27,15 @@ int main(int argc, char *argv[])
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
 	int n = 0;
+    int anzahl = 0;
+    char invalidNumBytes[] = "NOK! Invalid number of bytes!";
+
 	if (argc < 2)
 	{
 		fprintf(stderr,"ERROR, no port provided\n");
 		exit(1);
 	}
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
@@ -49,6 +53,7 @@ int main(int argc, char *argv[])
 	clilen = sizeof(cli_addr);
 	for(;;)
 	{
+        char invalid = 0;
 		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
 		//BEGIN get active socket port
@@ -60,7 +65,7 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			printf("port number %d\n", ntohs(sin.sin_port));
+			printf("Incoming connection at port: %d\n", ntohs(sin.sin_port));
 		}
 		//END get active socket port
 
@@ -69,7 +74,39 @@ int main(int argc, char *argv[])
 			error("ERROR on accept");
 		}
 
-		bzero(buffer,256);
+        memset(buffer, '\0', sizeof(buffer));
+
+        //Receive number of bytes to be read and sanitize them
+        n = read(newsockfd, buffer, 2);
+
+        for(int i = 0; i < strlen(buffer); i++)
+	    {
+            if(!(buffer[i] >= '0' && buffer[i] <= '9'))
+            {
+        	    printf("Invalid number of bytes!\n");
+                printf("Received Data: %s\n", buffer);
+                write(newsockfd, invalidNumBytes, strlen(invalidNumBytes));
+                invalid = 1;
+            }
+	    }
+
+        if(!invalid) {
+            anzahl = atoi(buffer);
+
+            if (anzahl > 10) {
+                printf("Too many bytes to be read!\n");
+                write(newsockfd, invalidNumBytes, strlen(invalidNumBytes));
+                invalid = 1;
+            }
+        }
+
+        printf("\n");
+
+        if(!invalid) {
+        n = write(newsockfd, "OK", 2);
+        printf("Number of bytes: %d\n", anzahl);
+
+        memset(buffer, '\0', sizeof(buffer));
 
 		//DATEN VOM CLIENT EMPFANGEN
 		n = read(newsockfd,buffer,255);
@@ -78,18 +115,18 @@ int main(int argc, char *argv[])
 
 		printf("Here is the message: %s\n\n",buffer);
 
-		n = write(newsockfd, "I got your message: %s\n",18);
+		n = write(newsockfd, "I got your message.\n",255);
 
 		if (n < 0) error("ERROR writing to socket");
 
-//		n = read(newsockfd,buffer,255);
-
-//		if (n < 0) error("ERROR reading from socket");
-
-//		printf("\nBuffer: %s\n\n", buffer);
-
 		// Buffer wieder aufteilen, pruefen und ausgeben
-		aufteilung = strtok(buffer, "@");
+		/** TOFIX **/
+		int i = 0; // Laufvariable
+		char newbuffer[256];
+        char outputString[10][20];
+		memset(newbuffer, '\0', sizeof(newbuffer));
+		/** TOFIX **/
+		aufteilung = strtok(buffer, "<");
 		do
 		{
 			printf("Moeglicher Dateiname gefunden: %s\n", aufteilung);
@@ -98,18 +135,37 @@ int main(int argc, char *argv[])
 			if(fp == NULL)
 			{
 					printf("Die Datei %s existiert NICHT.\n\n", aufteilung);
+					newbuffer[i] = '0';
 			} else
 			{
 					printf("Die Datei %s existiert.\n\n", aufteilung);
+					newbuffer[i] = '1';
+                    fgets(outputString[i], anzahl+1, fp);
 					fclose(fp);
 			}
 
-			aufteilung = strtok(NULL, "@");
+			aufteilung = strtok(NULL, "<");
+			i++;
 
 		} while(aufteilung != NULL);
 
+		n = write(newsockfd, newbuffer, 255);
+        usleep(1000);
+
+        for(int j = 0; j < i; j++) {
+            if(newbuffer[j] == '1') {
+            n = write(newsockfd, outputString[j], strlen(outputString[j]));
+            printf("String: %s\n", outputString[j]);
+            usleep(1000);
+            }
+        }
+        printf("-----------------------------\n");
+
+		if (n < 0) error("ERROR writing to socket");
+        }
 		close(newsockfd);
-	}
+        }
+
 	close(sockfd);
 	return 0;
 }
